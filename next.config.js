b68b2143903c764e863cc47e171b2e9a78710109
pavan <siriyalapavankumar@gmail.com>
2023-11-18 21:33:14 +0530

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
+module.exports = {
+  // Other configurations...
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, '/');
+    return config;
+  },
+};
 
-module.exports = nextConfig
